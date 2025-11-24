25a598c8b9bceee8373c82128b36163a27559568
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-11-24 12:26:46 +0700

    menambah kan code

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const { connectDatabase } = require("./config/db");
 
+const apiRoutes = require("./routes/api");
+
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", apiRoutes);
 
 async function startServer() {
   await connectDatabase();
